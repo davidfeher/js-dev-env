fac35056b6bef50ffb24a691a7bc81e1351a5ae7
david.feher <feherkeee@gmail.com>
2019-10-24 00:26:13 +0200

    removed unused empty wepback configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,25 +3,18 @@ const path = require('path');
 module.exports = {
   mode: "production", // "production" | "development" | "none"
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
-  entry: "./src", // string | object | array
+  entry: path.resolve(__dirname, 'src/index'), // string | object | array
   // defaults to ./src
   // Here the application starts executing
   // and webpack starts bundling
   output: {
     // options related to how webpack emits results
-    path: path.resolve(__dirname, "dist"), // string
+    path: path.resolve(__dirname, "src"), // string
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
     filename: "bundle.js", // string
     // the filename template for entry chunks
-    publicPath: "/assets/", // string
-    // the url to the output directory resolved relative to the HTML page
-    library: "MyLibrary", // string,
-    // the name of the exported library
-    libraryTarget: "umd", // universal module definition
-    // the type of the exported library
-    /* Advanced output configuration (click to show) */
-    /* Expert output configuration (on own risk) */
+    publicPath: "/"
   },
   module: {
     // configuration regarding modules
