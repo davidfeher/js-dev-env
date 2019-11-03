a04dc056d06d109b6fc763a4e4b1489695cc60f1
david.feher <feherkeee@gmail.com>
2019-11-03 17:59:43 +0100

    added api to simulate users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -14,7 +14,16 @@ app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
 }));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')))
+app.get('/users', function(req, res) {
+    // Hard coded for simplicity
+    res.json([
+        {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob.smith@gmail.com"},
+        {"id": 2, "firstName": "Alice", "lastName": "Jones", "email": "alice.jones@gmail.com"},
+        {"id": 3, "firstName": "Michael", "lastName": "Archangel", "email": "bible_is_fake@gmail.com"}
+    ])
+});
 
 app.listen(port, function(err) {
     if(err) {
